e9f43a6db4441a84849a079050049c598f15e5b1
dacanizares <dacanizares@outlook.com>
2020-06-08 18:21:31 -0500

    Adding logs on releasemode

--- src/SampleAPI/Startup.cs
+++ src/SampleAPI/Startup.cs
@@ -163,6 +163,7 @@ namespace SampleAPI
             }
             else
             {
+                app.UseDeveloperExceptionPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
